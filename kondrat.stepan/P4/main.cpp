2d2630f2ec53ea79f5ae6c4ce8a55b684c6b3164
StKondrat <kndrt13@icloud.com>
2025-11-22 21:14:44 +0300

    add getline func

--- kondrat.stepan/P4/main.cpp
+++ kondrat.stepan/P4/main.cpp
@@ -38,6 +38,53 @@ namespace kondrat
     delete[] small_str;
     return new_str;
   }
+
+  char * getline(std::istream & in, size_t & size)
+  {
+    bool is_skipws = in.flags() & std::ios_base::skipws;
+    if (is_skipws)
+    {
+      in >> std::noskipws;
+    }
+
+    size = 5;
+    char * buffer = create_str(size);
+
+    if (!buffer)
+    {
+      return nullptr;
+    }
+
+    size_t pos = 0;
+    int ch = 0;
+
+    while (true)
+    {
+      ch = in.get();
+      if (ch == '\n' || ch == EOF)
+      {
+        break;
+      }
+      if (pos + 1 >= size)
+      {
+        char * bigger = expand_str(buffer, size);
+        if (!bigger)
+        {
+          return nullptr;
+        }
+        buffer = bigger;
+        size += 5;
+      }
+      buffer[pos++] = static_cast< char >(ch);
+    }
+    buffer[pos] = '\0';
+
+    if(is_skipws)
+    {
+      in >> std::skipws;
+    }
+    return buffer;
+  }
 }
 
 int main()
