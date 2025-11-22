8116e38774c9af6a8c760bb3a59ef1f4396c9379
StKondrat <kndrt13@icloud.com>
2025-11-22 19:49:46 +0300

    add create_str and expand_str funcs

--- kondrat.stepan/P4/main.cpp
+++ kondrat.stepan/P4/main.cpp
@@ -1,2 +1,46 @@
+#include <iostream>
+#include <cstddef>
+#include <cctype>
+#include <iomanip>
+
+namespace kondrat
+{
+  char * create_str(size_t size)
+  {
+    char * str = nullptr;
+    try
+    {
+      str = new char[size];
+    }
+    catch(const std::bad_alloc& e)
+    {
+      std::cerr << "Memory allocation failed!\n";
+    }
+    return str;
+  }
+
+  char * expand_str(char * small_str, size_t small_size)
+  {
+    size_t new_size = small_size + 5;
+    char * new_str = nullptr;
+    try
+    {
+      new_str = new char[new_size];
+    }
+    catch(const std::bad_alloc& e)
+    {
+      std::cerr << "Memory allocation failed!\n";
+    }
+    for (size_t i = 0; i < small_size; ++i)
+    {
+      new_str[i] = small_str[i];
+    }
+    delete[] small_str;
+    return new_str;
+  }
+}
+
 int main()
-{}
+{
+
+}
