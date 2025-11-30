20a9e4c94c1617f1938a0c14b651df54762c0c13
Кудаев Георгий Александрович <monkey-george@rambler.ru>
2025-11-30 13:43:03 +0300

    TO4NO fixed content

--- kudaev.georgii/P4/main.cpp
+++ kudaev.georgii/P4/main.cpp
@@ -60,131 +60,131 @@ int main()
 
 size_t kudaev::getline(std::istream& in, char* str, size_t length, size_t capacity)
 {
-	in >> std::noskipws;
-	char ch;
-	while (in >> ch)
-	{
-		if (ch == '\n')
-		{
-			break;
-		}
-		str[length++] = ch;
-		if (length >= capacity - 1)
-		{
-			return length;
-		}
-	}
-	str[length] = '\0';
-	return length;
+  in >> std::noskipws;
+  char ch;
+  while (in >> ch)
+  {
+    if (ch == '\n')
+    {
+      break;
+    }
+    str[length++] = ch;
+    if (length >= capacity - 1)
+    {
+      return length;
+    }
+  }
+  str[length] = '\0';
+  return length;
 }
 
 size_t kudaev::strlen(const char* str)
 {
-	if (str == nullptr)
-	{
-		return 0;
-	}
-	size_t length = 0;
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-	return length;
+  if (str == nullptr)
+  {
+    return 0;
+  }
+  size_t length = 0;
+  while (str[length] != '\0')
+  {
+    length++;
+  }
+  return length;
 }
 
 char* kudaev::getstr(char* str)
 {
-	size_t capacity = 2, length = 0;
-	try
-	{
-		str = (char*)malloc(capacity * sizeof(char));
-		if (!str)
-		{
-			throw;
-		}
-		while (true)
-		{
-			length = getline(std::cin, str, length, capacity);
-			if (length < capacity - 1 || std::cin.eof())
-			{
-				break;
-			}
-			capacity *= 2;
-			char* new_str = (char*)malloc(capacity * sizeof(char));
-			if (!new_str)
-			{
-				free(str);
-				throw;
-			}
-			for (size_t i = 0; i < length; ++i)
-			{
-				new_str[i] = str[i];
-			}
-			free(str);
-			str = new_str;
-		}
-	}
-	catch (...)
-	{
-		if (str)
-		{
-			free(str);
-			str = nullptr;
-		}
-		return nullptr;
-	}
-	return str;
+  size_t capacity = 2, length = 0;
+  try
+  {
+    str = (char*)malloc(capacity * sizeof(char));
+    if (!str)
+    {
+       throw;
+    }
+    while (true)
+    {
+      length = getline(std::cin, str, length, capacity);
+      if (length < capacity - 1 || std::cin.eof())
+      {
+        break;
+      }
+      capacity *= 2;
+      char* new_str = (char*)malloc(capacity * sizeof(char));
+      if (!new_str)
+      {
+        free(str);
+        throw;
+      }
+      for (size_t i = 0; i < length; ++i)
+      {
+        new_str[i] = str[i];
+      }
+      free(str);
+      str = new_str;
+    }
+  }
+  catch (...)
+  {
+    if (str)
+    {
+      free(str);
+      str = nullptr;
+    }
+    return nullptr;
+  }
+  return str;
 }
 
 size_t kudaev::has_same(const char* str1, const char* str2)
 {
-	if (str1 == nullptr || str2 == nullptr)
-	{
-		return 0;
-	}
-	size_t count = 0;
-	size_t len1 = strlen(str1);
-	size_t len2 = strlen(str2);
-	for (size_t i = 0; i < len1; ++i)
-	{
-		for (size_t j = 0; j < len2; ++j)
-		{
-			if (str1[i] == str2[j])
-			{
-				count++;
-			}
-		}
-	}
-	return count;
+  if (str1 == nullptr || str2 == nullptr)
+  {
+    return 0;
+  }
+  size_t count = 0;
+  size_t len1 = strlen(str1);
+  size_t len2 = strlen(str2);
+  for (size_t i = 0; i < len1; ++i)
+  {
+    for (size_t j = 0; j < len2; ++j)
+    {
+      if (str1[i] == str2[j])
+      {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 char* kudaev::lat_rmv(const char* str)
 {
-	if (str == nullptr)
-	{
-		return nullptr;
-	}
-	size_t count = 0;
-	for (size_t i = 0; str[i] != '\0'; ++i)
-	{
-		if (!isalpha(str[i]))
-		{
-			count++;
-		}
-	}
-	char* new_str = (char*)malloc((count + 1) * sizeof(char));
-	if (!new_str)
-	{
-		return nullptr;
-	}
-	size_t j = 0;
-	for (size_t i = 0; str[i] != '\0'; ++i)
-	{
-		if (!isalpha(str[i]))
-		{
-			new_str[j++] = str[i];
-		}
-	}
-	new_str[j] = '\0';
-	return new_str;
+  if (str == nullptr)
+  {
+    return nullptr;
+  }
+  size_t count = 0;
+  for (size_t i = 0; str[i] != '\0'; ++i)
+  {
+    if (!isalpha(str[i]))
+    {
+      count++;
+    }
+  }
+  char* new_str = (char*)malloc((count + 1) * sizeof(char));
+  if (!new_str)
+  {
+    return nullptr;
+  }
+  size_t j = 0;
+  for (size_t i = 0; str[i] != '\0'; ++i)
+  {
+    if (!isalpha(str[i]))
+    {
+      new_str[j++] = str[i];
+    }
+  }
+  new_str[j] = '\0';
+  return new_str;
 }
