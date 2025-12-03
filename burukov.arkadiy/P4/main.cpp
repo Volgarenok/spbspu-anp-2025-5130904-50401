95baffeed91f6e6683c27ab9ae458f6e813bf3ef
Deflnam <burukov01@bk.ru>
2025-12-03 21:43:03 +0300

    Add basic string utilities and namespace structure.

--- burukov.arkadiy/P4/main.cpp
+++ burukov.arkadiy/P4/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cstddef>
+#include <limits>
+
+namespace burukov
+{
+  size_t stringLength(const char* str);
+  const size_t MAX = std::numeric_limits< size_t >::max();
+}
+
+size_t burukov::stringLength(const char* str)
+{
+  size_t count = 0;
+  while (str[count] != '\0')
+  {
+    ++count;
+  }
+  return count;
+}
 
 int main()
 {
-    
+  return 0;
 }
