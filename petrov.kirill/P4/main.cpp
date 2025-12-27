4c47ec4cfc18eef33066333b0b27136c026a8405
kirill57701 <kirillpetrov56333@gmail.com>
2025-12-27 22:22:45 +0300

    Update main.cpp

--- petrov.kirill/P4/main.cpp
+++ petrov.kirill/P4/main.cpp
@@ -4,7 +4,32 @@
 
 namespace petrov {
   void getline(std::istream& in, const size_t size, char* str, size_t& k);
+  void rpl_sym(size_t& k, char* new_str, char* rpl_sym_str, char a, char b);
+  void lat_two(size_t& s, size_t& k, size_t& r, size_t& ch, char* lat_two_str, char* new_str, char* alph);
+  bool is_el_in_mass(size_t& k, char* mass, char a);
 };
+
+bool petrov::is_el_in_mass(size_t& k, char* mass, char a) {
+  for (size_t i = 0; i < k; ++i) {
+    if (mass[i] == a) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void petrov::lat_two(size_t& s, size_t& k, size_t& r, size_t& ch, char* lat_two_str, char* new_str, char* alph) {
+  for (size_t i = 0; i < s; ++i) {
+    if (!petrov::is_el_in_mass(ch, alph, lat_two_str[i])) {
+      alph[r++] = lat_two_str[i];
+    }
+  }
+  for (size_t i = 0; i < k; ++i) {
+    if (!petrov::is_el_in_mass(ch, alph, new_str[i])) {
+      alph[r++] = new_str[i];
+    }
+  }
+}
 
 void petrov::getline(std::istream& in, const size_t size, char* str, size_t& k) {
   std::cin >> std::noskipws;
@@ -13,6 +38,17 @@ void petrov::getline(std::istream& in, const size_t size, char* str, size_t& k) 
       throw std::logic_error("err");
     }
     in >> str[k++];
+  }
+  std::cin >> std::skipws;
+}
+
+void petrov::rpl_sym(size_t& k, char* new_str, char* rpl_sym_str, char a, char b) {
+  for (size_t i = 0; i < k; ++i) {
+    if (new_str[i] == a) {
+      rpl_sym_str[i] = b;
+    } else {
+      rpl_sym_str[i] = new_str[i];
+    }
   }
 }
 
@@ -37,9 +73,43 @@ int main() {
     new_str[i] = str[i];
   }
   delete[] str;
+  char a, b;
+  std::cin >> a >> b;
+  char* rpl_sym_str = new char[k];
+  petrov::rpl_sym(k, new_str, rpl_sym_str, a, b);
+  char* new_str_2 = new char[size];
+  size_t s = 0;
+  try {
+    petrov::getline(std::cin, size, new_str_2, s);
+  } catch (...) {
+    std::cerr << "err\n";
+    delete[] rpl_sym_str;
+    delete[] new_str;
+    delete[] new_str_2;
+    return 1;
+  }
+  char* lat_two_str = new char[s];
+  for (size_t i = 0; i < s; ++i) {
+    lat_two_str[i] = new_str_2[i];
+  }
+  delete[] new_str_2;
+  size_t r = 0, ch = 52;
+  char* alph = new char[ch];
+  petrov::lat_two(s, k, r, ch, lat_two_str, new_str, alph);
   for (size_t i = 0; i < k; ++i) {
     std::cout << new_str[i];
   }
+  std::cout << "\n";
+  for (size_t i = 0; i < k; ++i) {
+    std::cout << rpl_sym_str[i];
+  }
+  std::cout << "\n";
+  for (size_t i = 0; i < ch; ++i) {
+    std::cout << alph[i];
+  }
+  delete[] lat_two_str;
+  delete[] alph;
+  delete[] rpl_sym_str;
   delete[] new_str;
   return 0;
 }
