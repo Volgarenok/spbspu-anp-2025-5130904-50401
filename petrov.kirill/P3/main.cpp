5e8573e00937cff47eb494f306e2e8410fabf2cf
kirill57701 <kirillpetrov56333@gmail.com>
2026-01-14 14:39:34 -0400

    Update

--- petrov.kirill/P3/main.cpp
+++ petrov.kirill/P3/main.cpp
@@ -61,10 +61,10 @@ int* petrov::make_mtx(std::ifstream& in, size_t r, size_t c, char t, int* statmt
     {
       s = petrov::fill_massive(r, in, mtx, s, t);
     }
-    catch (const std::logic_error& e)
+    catch (...)
     {
       free(mtx);
-      throw e;
+      throw std::logic_error("err\n");
     }
     for (size_t i = r * r; i < w; ++i)
     {
