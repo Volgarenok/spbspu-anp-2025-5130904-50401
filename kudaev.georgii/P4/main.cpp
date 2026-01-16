9c3b9c40286115f8feef429590310fdc524c7156
Кудаев Георгий Александрович <monkey-george@rambler.ru>
2026-01-16 22:36:09 +0300

    changed file

--- kudaev.georgii/P4/main.cpp
+++ kudaev.georgii/P4/main.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <cctype>
 
 namespace kudaev
 {
   size_t getLine(std::istream&, char*, size_t, size_t);
-  size_t strLen(const char*);
   char* getStr(char*);
   size_t hasSame(const char*, const char*);
-  void latRmv(const char*, char*, size_t);
+  char* latRmv(const char*);
 }
 
 int main()
 {
-  char* str1 = nullptr;
+  char* str1 = kudaev::getStr(str1);
   const char* str2 = "Ilovecpp";
   const char* str3 = "abs213fsd";
-  str1 = kudaev::getStr(str1);
   if (!str1)
   {
     std::cerr << "Failed to read input\n";
-    if (str1)
-    {
-      free(str1);
-    }
     return 1;
   }
   std::cout << kudaev::hasSame(str1, str2) << '\n';
-  size_t buffer_size = kudaev::strLen(str3) + 1;
-  char* buffer = static_cast< char* >(malloc(buffer_size));
+  char* buffer = kudaev::latRmv(str3);
   if (!buffer)
   {
     std::cerr << "Memory allocation failed\n";
     free(str1);
     return 1;
   }
-  kudaev::latRmv(str3, buffer, buffer_size);
   std::cout << buffer << '\n';
   free(buffer);
   free(str1);
@@ -44,7 +36,8 @@ int main()
 
 size_t kudaev::getLine(std::istream& in, char* str, size_t length, size_t capacity)
 {
-  in >> std::noskipws;
+  bool needSkipws = (in.flags() & std::ios::skipws) != 0;
+  in.unsetf(std::ios::skipws);
   char ch;
   while (in >> ch)
   {
@@ -59,20 +52,9 @@ size_t kudaev::getLine(std::istream& in, char* str, size_t length, size_t capaci
     }
   }
   str[length] = '\0';
-  in >> std::skipws;
-  return length;
-}
-
-size_t kudaev::strLen(const char* str)
-{
-  if (str == nullptr)
+  if (needSkipws)
   {
-    return 0;
-  }
-  size_t length = 0;
-  while (str[length] != '\0')
-  {
-    length++;
+    in.setf(std::ios::skipws);
   }
   return length;
 }
@@ -80,47 +62,35 @@ size_t kudaev::strLen(const char* str)
 char* kudaev::getStr(char* str)
 {
   size_t capacity = 2, length = 0;
-  try
+  str = static_cast< char* >(malloc(capacity * sizeof(char)));
+  if (!str)
   {
-    str = static_cast< char* >(malloc(capacity * sizeof(char)));
-    if (!str)
-    {
-      throw;
-    }
-    while (!std::cin.eof())
-    {
-      length = getLine(std::cin, str, length, capacity);
-      if (length < capacity - 1)
-      {
-        break;
-      }
-      capacity *= 2;
-      char* new_str = static_cast< char* >(malloc(capacity * sizeof(char)));
-      if (!new_str)
-      {
-        free(str);
-        throw;
-      }
-      for (size_t i = 0; i < length; ++i)
-      {
-        new_str[i] = str[i];
-      }
-      free(str);
-      str = new_str;
-    }
-  }
-  catch (...)
-  {
-    if (str)
-    {
-      str = nullptr;
-    }
     return nullptr;
+  }
+  while (!std::cin.eof())
+  {
+    length = getLine(std::cin, str, length, capacity);
+    if (length < capacity - 1)
+    {
+      break;
+    }
+    capacity *= 2;
+    char* new_str = static_cast< char* >(malloc(capacity * sizeof(char)));
+    if (!new_str)
+    {
+      free(str);
+      return nullptr;
+    }
+    for (size_t i = 0; i < length; ++i)
+    {
+      new_str[i] = str[i];
+    }
+    free(str);
+    str = new_str;
   }
   if (length == 0)
   {
     free(str);
-    str = nullptr;
     return nullptr;
   }
   return str;
@@ -133,11 +103,9 @@ size_t kudaev::hasSame(const char* str1, const char* str2)
     return 0;
   }
   size_t count = 0;
-  size_t len1 = strLen(str1);
-  size_t len2 = strLen(str2);
-  for (size_t i = 0; i < len1; ++i)
+  for (size_t i = 0; str1[i] != '\0'; ++i)
   {
-    for (size_t j = 0; j < len2; ++j)
+    for (size_t j = 0; str2[j] != '\0'; ++j)
     {
       if (str1[i] == str2[j])
       {
@@ -148,14 +116,27 @@ size_t kudaev::hasSame(const char* str1, const char* str2)
   return count;
 }
 
-void kudaev::latRmv(const char* src, char* dest, size_t dest_size)
+char* kudaev::latRmv(const char* src)
 {
-  if (src == nullptr || dest == nullptr || dest_size == 0)
+  if (src == nullptr)
   {
-    return;
+    return nullptr;
+  }
+  size_t count = 0;
+  for (size_t i = 0; src[i] != '\0'; ++i)
+  {
+    if (!std::isalpha(static_cast< unsigned char >(src[i])))
+    {
+      count++;
+    }
+  }
+  char* dest = static_cast< char* >(malloc(count + 1));
+  if (!dest)
+  {
+    return nullptr;
   }
   size_t j = 0;
-  for (size_t i = 0; src[i] != '\0' && j < dest_size - 1; ++i)
+  for (size_t i = 0; src[i] != '\0'; ++i)
   {
     if (!std::isalpha(static_cast< unsigned char >(src[i])))
     {
@@ -163,4 +144,5 @@ void kudaev::latRmv(const char* src, char* dest, size_t dest_size)
     }
   }
   dest[j] = '\0';
+  return dest;
 }
