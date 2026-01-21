cf6c00df68482d69a1b1289fbba89098cde5cf78
Petrov Vadim <vadikdydy@mail.ru>
2026-01-21 18:59:51 +0300

    rm big lines

--- petrov.vadim/P3/main.cpp
+++ petrov.vadim/P3/main.cpp
@@ -8,8 +8,8 @@ namespace petrov
   bool fill(std::istream& input, int* mtx, size_t rows, size_t cols);
   int* copy(int* mtx, size_t rows, size_t cols);
   void lft_bot_cnt(std::ostream& output, int* mtx, size_t rows, size_t cols);
-  void vert_step_for_task1(int* mtx, size_t top, size_t bottom, size_t right, size_t left, size_t& plus_step, bool move_down, size_t cols);
-  void hor_step_for_task1(int* mtx, size_t top, size_t bottom, size_t right, size_t left, size_t& plus_step, bool move_right, size_t cols);
+  void vert_step_for_task1(int* mtx, size_t t, size_t b, size_t r, size_t l, size_t& step, bool mv_d, size_t cols);
+  void hor_step_for_task1(int* mtx, size_t t, size_t b, size_t r, size_t l, size_t& step, bool mv_r, size_t cols);
   void fll_inc_wav(std::ostream& output, int* mtx, size_t rows, size_t cols);
   void vert_step_for_task2(int* mtx, size_t col, size_t row_start, size_t row_end, size_t cols, size_t plus_step);
   void hor_step_for_task2(int* mtx, size_t row, size_t col_start, size_t col_end, size_t cols, size_t plus_step);
@@ -68,38 +68,38 @@ bool petrov::fill(std::istream& input, int* mtx, size_t rows, size_t cols)
   return true;
 }
 
-void petrov::vert_step_for_task1(int* mtx, size_t top, size_t bottom, size_t right, size_t left, size_t& plus_step, bool move_down, size_t cols)
+void petrov::vert_step_for_task1(int* mtx, size_t t, size_t b, size_t r, size_t l, size_t& step,bool mv_d,size_t cols)
 {
-  if (move_down)
+  if (mv_d)
   {
-    for (size_t i = top; i <= bottom; ++i)
+    for (size_t i = t; i <= b; ++i)
     {
-      mtx[i * cols + left] += plus_step++;
+      mtx[i * cols + l] += step++;
     }
   }
   else
   {
-    for (size_t i = bottom + 1; i-- > top;)
+    for (size_t i = b + 1; i-- > t;)
     {
-      mtx[i * cols + right] += plus_step++;
+      mtx[i * cols + r] += step++;
     }
   }
 }
 
-void petrov::hor_step_for_task1(int* mtx, size_t top, size_t bottom, size_t right, size_t left, size_t& plus_step, bool move_right, size_t cols)
+void petrov::hor_step_for_task1(int* mtx, size_t t, size_t b, size_t r, size_t l, size_t& step,bool mv_r, size_t cols)
 {
-  if (move_right)
+  if (mv_r)
   {
-    for (size_t i = left; i <= right; ++i)
+    for (size_t i = l; i <= r; ++i)
     {
-      mtx[bottom * cols + i] += plus_step++;
+      mtx[b * cols + i] += step++;
     }
   }
   else
   {
-    for (size_t i = right + 1; i-- > left;)
+    for (size_t i = r + 1; i-- > l;)
     {
-      mtx[top * cols + i] += plus_step++;
+      mtx[t * cols + i] += step++;
     }
   }
 }
